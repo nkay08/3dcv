90c46e929f2c7bc05994ac5b3c203dcb9fb4b9f6
NKay <niklas.klipphahn@gmail.com>
2018-01-28 17:21:11 +0100

    ex05: errors in original files fixed?

--- ex05/flowers.py
+++ ex05/flowers.py
@@ -83,7 +83,7 @@ def ply_creator(input_, filename):
 			if i!=input_.shape[0]-1:
 				fid.write('\n')
 	fid.close()
-    return True
+	return True
 """
 sub_pixel_crop() and pixel_interpolate(): are provided incase you want to do sub pixel matching.
 sub_pixel_crop() will allow you to crop a patch centered at a floating point location like (10.89, 3.88).
@@ -131,38 +131,38 @@ def copy_make_border(img, K_SIZE):
 	return cv.copyMakeBorder(img, top=offset, bottom=offset, left=offset, right=offset, borderType=cv.BORDER_REFLECT)
 
 def write_depth_to_image(depth, f_name):
-    """
-    This function writes depth map to f_name
-    """
-    assert (input_.ndim==2),"Depth map should be a 2D array "
+	"""
+	This function writes depth map to f_name
+	"""
+	#assert (input_.ndim==2),"Depth map should be a 2D array "
 	max_depth = np.max(depth)
 	min_depth = np.min(depth)
 	depth_v1 = 255 - 255*((depth-min_depth)/max_depth)
-    #depth_v2 = 255*((depth-min_depth)/max_depth)
+	#depth_v2 = 255*((depth-min_depth)/max_depth)
 	cv.imwrite(f_name, depth_v1)
 	return True
 
 def depth_to_3d(Z, y, x):
-    """
-    Given the pixel position(y,x) and depth(Z)
-    It computes the 3d point in world coordinates,
-    first back-project the point from homogeneous image space to 3D,  by multiplying it with inverse of the camera intrinsic matrix,  inv(K)
-    Then scale it so that you will get the point at depth equal to Z.
-    the scale the vector
-    """
+	"""
+	Given the pixel position(y,x) and depth(Z)
+	It computes the 3d point in world coordinates,
+	first back-project the point from homogeneous image space to 3D,  by multiplying it with inverse of the camera intrinsic matrix,  inv(K)
+	Then scale it so that you will get the point at depth equal to Z.
+	the scale the vector
+	"""
 	X_W = Z*np.dot(np.linalg.inv(K_LEFT),np.asarray([[x],[y],[1]]))
 	X_W = X_W.reshape(3,1)
-    # returns a list [X, Y, Z]
-    return [X_W[0,0], X_W[1,0], X_W[2,0]]
+	# returns a list [X, Y, Z]
+	return [X_W[0,0], X_W[1,0], X_W[2,0]]
 
 def is_outlier(score_vector):
-    """
-    O_F_THRESHOLD is the Outlier Filtering Threshold.
-    For 'ncc' metric: if more than O_F_THRESHOLD number of disparity values have ncc score greater of equal to 0.8*max_score, the function returns True.
-    For 'ssd' metric: if more than O_F_THRESHOLD number of disparity values have ssd score less than of equal to 1.4*min_score, the function returns True.
-    The above thresholds are chosen by the TAs. Feel free, to try out different values.
-    - The input to this function should be a 1D array of ncc scores computed for all disparities.
-    """
+	"""
+	O_F_THRESHOLD is the Outlier Filtering Threshold.
+	For 'ncc' metric: if more than O_F_THRESHOLD number of disparity values have ncc score greater of equal to 0.8*max_score, the function returns True.
+	For 'ssd' metric: if more than O_F_THRESHOLD number of disparity values have ssd score less than of equal to 1.4*min_score, the function returns True.
+	The above thresholds are chosen by the TAs. Feel free, to try out different values.
+	- The input to this function should be a 1D array of ncc scores computed for all disparities.
+	"""
 	assert (score_vector.ndim==1), "Pass 1D array of vector of scores "
 	if SIMILARITY_MERTIC=='ncc':
 		max_score = np.max(score_vector)
@@ -172,6 +172,7 @@ def is_outlier(score_vector):
 			return False
 	if SIMILARITY_MERTIC=='ssd':
 		min_distance = np.min(score_vector)
+		min_score=min_distance
 		if np.sum(score_vector<=1.4*min_score)>=O_F_THRESHOLD:
 			return True
 		else:
@@ -179,54 +180,77 @@ def is_outlier(score_vector):
 	return True
 
 def disparity_to_depth(disparity):
-    """
-    Converts disparity to depth.
-    """
+	"""
+	Converts disparity to depth.
+	"""
 	inv_depth = (disparity+DOFFS)/(BASELINE*F)
 	return 1/inv_depth
 
 def ncc(patch_1, patch_2):
-    """
-    TODO
-    1. Normalise the input patch by subtracting its mean and dividing by its standard deviation.
-    Perform the normalisation for each color channel of the input patch separately.
-    2. Reshape each normalised image patch into a 1D feature vector and then
-    compute the dot product between the resulting normalised feature vectors.
-    """
-    raise NotImplementedError
+	"""
+	TODO
+	1. Normalise the input patch by subtracting its mean and dividing by its standard deviation.
+	Perform the normalisation for each color channel of the input patch separately.
+	2. Reshape each normalised image patch into a 1D feature vector and then
+	compute the dot product between the resulting normalised feature vectors.
+	"""
+	raise NotImplementedError
 
 def ssd(feature_1, feature_2):
-    """
-    TODO
-    Compute the sum of square difference between the input features
-    """
+	"""
+	TODO
+	Compute the sum of square difference between the input features
+	"""
+
 	raise NotImplementedError
 
 def stereo_matching(img_left, img_right, K_SIZE, disp_per_pixel):
 	"""
-    TODO
-    This is the main function that you have to write.
-    For the region of the left image delimited by H_BOUND and H_BOUND, this section should do the following.
-        1. Compute Depth-Map and save it as a gray scale
-        2. Create point cloud by giving depth map to depth_to_3d() function.
-        3. Using ply_creator(), save point cloud:
-    Note that: the left and right_images are padded; by int(K_SIZE/2) on the left, right, top and bottom.
-    """
-    # TASKs:
-    # Iterate over every pixel of the left image (with in the region bounded by H_BOUND and W_BOUND), (lets denote a sample as P_x_y)
-        # For every P_x_y:
-         # compute disparity using ncc cost metric
-         # Convert disparity to depth, use disparity_to_depth()
-         # Convert depth to point cloud, use depth_to_3d()
-    # Save point clouds as .ply and depth as .png files
-    raise NotImplementedError
-    return True
+	TODO
+	This is the main function that you have to write.
+	For the region of the left image delimited by H_BOUND and H_BOUND, this section should do the following.
+		1. Compute Depth-Map and save it as a gray scale
+		2. Create point cloud by giving depth map to depth_to_3d() function.
+		3. Using ply_creator(), save point cloud:
+	Note that: the left and right_images are padded; by int(K_SIZE/2) on the left, right, top and bottom.
+	"""
+	# TASKs:
+	# Iterate over every pixel of the left image (with in the region bounded by H_BOUND and W_BOUND), (lets denote a sample as P_x_y)
+		# For every P_x_y:
+		 # compute disparity using ncc cost metric
+		 # Convert disparity to depth, use disparity_to_depth()
+		 # Convert depth to point cloud, use depth_to_3d()
+	# Save point clouds as .ply and depth as .png files
+	#
+	#for every row in im_left
+	dispMap=np.zeros((H_BOUND[1]-H_BOUND[0],W_BOUND[1]-W_BOUND[0]))
+	for i in range(H_BOUND[0],H_BOUND[1]):
+		#for every column in im_left
+		#->for every pixel in im_left within bounds
+		for j0 in range(W_BOUND[0],W_BOUND[1]):
+			P0 = img_left[i,j0]
+
+			#for every pixel in im_left look for pixel in right image on same row
+
+			for j1 in range(W_BOUND[0],W_BOUND[1]):
+				P1 = img_right[i,j1]
+				ssd= ssd(P0,P1)
+	#cv.imshow("disp",dispMap)
+	#cv.waitKey(0)
+	#raise NotImplementedError
+	return True
+
+def pixelValue(pixel):
+	weightR= 0.3
+	weightG=0.6
+	weightB=0.11
+	return(weightR * pixel[0]+weightG * pixel[1]+ weightB * pixel[2])
 
 def main():
 	# Set parameters
 	# Use 1: for pixel-wise disparity
 	# Optional: you can try to use sub-pixel level disparity, using, disp_per_pixel = n, n>1
-    # Do not try this until you see the result for n=1
+	# Do not try this until you see the result for n=1
 	disp_per_pixel = 1
 	# Read images and expand borders
 	# load Left image
